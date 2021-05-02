abb824e88d3bb5eb9ac6e52c71f9162df9156f6d
Jared Connor <jaredconnor301@gmail.com>
2021-05-02 14:07:38 -0700

    First working production build

--- app.py
+++ app.py
@@ -1,19 +1,28 @@
-from flask import Flask
-from flask_restful import Api, Resource, abort
-from bs4 import BeautifulSoup
-import csv, requests
-import pandas
-import lxml
+from flask import Flask, request, jsonify 
+import requests
+import json
+import pandas as pd
+from pandas import json_normalize
  
 app = Flask(__name__) 
-api = Api(app)
+# api = Api(app)
 
-class table_filter(Resource): 
-    def get(self): 
-        return {'hello': 'world'}
+@app.route('/')
+def root(): 
+    sample_text = 'Simple microservice to pull data via https://cs361-table-scraper.herokuapp.com/ service'
 
+    wiki_page = request.args.get('wiki_page')
+    table_num = request.args.get('table_num')
+    num_rows = int(request.args.get('num_rows'))
 
-api.add_resource(table_filter, '/')
+    response = requests.get(f'https://cs361-table-scraper.herokuapp.com/{wiki_page}/{table_num}')
+    data = response.json()
+
+    df = pd.DataFrame.from_dict(data)
+    filter_df = df.head(num_rows)
+    json_data = filter_df.to_dict()
+
+    return jsonify(json_data)
 
 if __name__ == '__main__': 
     app.run(debug=True)
