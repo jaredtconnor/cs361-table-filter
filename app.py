5b2210466d324005d2b109cc9b4b28b6a076495b
Jared Connor <jaredconnor301@gmail.com>
2021-06-17 07:11:03 -0700

    Changing to flask_restful

--- app.py
+++ app.py
@@ -1,28 +1,41 @@
-from flask import Flask, request, jsonify 
+from flask import Flask, render_template, jsonify 
+from flask_restful import Api, Resource, abort
 import requests
-import json
 import pandas as pd
 from pandas import json_normalize
  
 app = Flask(__name__) 
-# api = Api(app)
+api = Api(app)
+
+def check_link(request): 
+    if request.status > 200: 
+        abort(400, message="Connection Error")
+    else: 
+        return True
+
+class filter_table(Resource): 
+
+    def root(self, search, table_num, top_n): 
+
+        response = requests.get('https://wikipedia-table-scraper.herokuapp.com/search/{0}/{1}').format(search, table_num)
+        data = response.json()
+
+        if top_n == None: 
+
+            json_data = pd.DataFrame.from_dict(data) 
+
+        else: 
+            df = pd.DataFrame.from_dict(data)
+            filter_df = df.head(top_n)
+            json_data = filter_df.to_dict()
+
+        return jsonify(json_data)
 
 @app.route('/')
-def root(): 
-    sample_text = 'Simple microservice to pull data via https://cs361-table-scraper.herokuapp.com/ service'
+def description(): 
+    return ("This is a simple microservice that pulls and filters Wikipedia tables.")
 
-    wiki_page = request.args.get('wiki_page')
-    table_num = request.args.get('table_num')
-    num_rows = int(request.args.get('num_rows'))
+api.add_resource(filter_table, "/filter/<string:search>/<int:table_num>/<int:top_n>")
 
-    response = requests.get(f'https://cs361-table-scraper.herokuapp.com/{wiki_page}/{table_num}')
-    data = response.json()
-
-    df = pd.DataFrame.from_dict(data)
-    filter_df = df.head(num_rows)
-    json_data = filter_df.to_dict()
-
-    return jsonify(json_data)
-    
 if __name__ == '__main__': 
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
